da57be6b7f933af46208a6a4ff77d01e58a49458
Lloyd <lloydmeta@gmail.com>
2016-10-18 10:35:28 +0900

    0.0.2 adds Monoid and semi group for tuples

--- src/monoid.rs
+++ src/monoid.rs
@@ -189,7 +189,7 @@ mod tests {
         let t3 = (1, 2.5f32, String::from(", goodbye"), Some(10));
         let tuples = vec![t1, t2, t3];
 
-        let expected = (3, 7.5f32, String::from("hi world, goodbye"), Some(133));
+        let expected = (3, 7.5f32, String::from("hi world, goodbye"), Some(13));
         assert_eq!(combine_all(&tuples), expected)
     }
 
