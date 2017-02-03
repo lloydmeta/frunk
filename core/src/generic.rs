f2e665ccc61c0c8aabc6ea3525ab7d5c13dfbcdc
Lloyd <lloydmeta@gmail.com>
2017-02-04 01:35:13 +0900

    Add a conversion helper method

--- core/src/generic.rs
+++ core/src/generic.rs
@@ -4,12 +4,18 @@ pub trait Generic<Repr> {
     fn from(r: Repr) -> Self;
 }
 
-/// Given a generic representation of an A, returns A
+/// Given a generic Representation of an A, returns A
 pub fn from_generic<A, Repr>(gen: Repr) -> A where A: Generic<Repr> {
     <A as Generic<Repr>>::from(gen)
 }
 
-/// Given an A, returns its generic representation
+/// Given an A, returns its generic Representation
 pub fn into_generic<A, Repr>(a: A) -> Repr where A: Generic<Repr> {
     <A as Generic<Repr>>::into(a)
+}
+
+/// Converts one type into another assuming they have the same generic Representation
+pub fn convert_from<A, B, Repr>(a: A) -> B where A: Generic<Repr>, B: Generic<Repr> {
+    let repr = <A as Generic<Repr>>::into(a);
+    <B as Generic<Repr>>::from(repr)
 }
