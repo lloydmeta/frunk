e192aa3c91bcc3ee92858216cc94438af3029f5f
Lloyd <lloydmeta@gmail.com>
2017-02-03 22:59:36 +0900

    It works ish

--- derives/src/lib.rs
+++ derives/src/lib.rs
@@ -5,10 +5,8 @@ extern crate frunk_core;
 extern crate quote;
 extern crate syn;
 
-use syn::{Ident, Body, Variant, VariantData, Field, Ty};
+use syn::{Ident, Body, VariantData, Field, Ty};
 use proc_macro::TokenStream;
-use frunk_core::hlist::*;
-use frunk_core::generic::*;
 
 #[proc_macro_derive(Generic)]
 pub fn generic(input: TokenStream) -> TokenStream {
@@ -36,72 +34,77 @@ fn impl_generic(ast: &syn::MacroInput) -> quote::Tokens {
     };
     let field_types: Vec<Ty> = fields.iter()
         .map(|f| f.ty.clone()).collect();
-    let fnames1: Vec<Ident> = fields
+    let repr_type = build_repr(&field_types);
+    let fnames: Vec<Ident> = fields
         .iter()
         .enumerate()
         .map(|(i, f)| f.ident.clone().unwrap_or(Ident::new(format!("_{}", i))))
         .collect();
-    let fnames2: Vec<Ident> = fields
+    let hcons_constr = build_hcons_constr(&fnames);
+    let hcons_pat = build_hcons_constr(&fnames);
+    let maybe_fnames: Vec<Option<Ident>> = fields
         .iter()
-        .enumerate()
-        .map(|(i, f)| f.ident.clone().unwrap_or(Ident::new(format!("_{}", i))))
+        .map(|f| f.ident.clone())
         .collect();
-    let fnames3: Vec<Ident> = fields
-        .iter()
-        .enumerate()
-        .map(|(i, f)| f.ident.clone().unwrap_or(Ident::new(format!("_{}", i))))
-        .collect();
-    let fnames4: Vec<Ident> = fields
-        .iter()
-        .enumerate()
-        .map(|(i, f)| f.ident.clone().unwrap_or(Ident::new(format!("_{}", i))))
-        .collect();
-    let fnames5: Vec<Ident> = fields
-        .iter()
-        .enumerate()
-        .map(|(i, f)| f.ident.clone().unwrap_or(Ident::new(format!("_{}", i))))
-        .collect();
+    let new_struct_constr = build_new_struct_constr(name, &maybe_fnames, &fnames);
 
     quote! {
         impl #impl_generics ::frunk_core::generic::Generic for #name #ty_generics #where_clause {
-            type Repr = ::frunk_core::hlist::Hlist![#(#field_types),*];
+            type Repr = #repr_type;
 
             fn into_generic(self) -> Self::Repr {
-                let #name { #(#fnames1, )* } = self;
-                ::frunk_core::hlist::hlist!(#(#fnames2),*)
+                let #name { #(#fnames, )* } = self;
+                #hcons_constr
             }
 
             fn from_generic(r: Self::Repr) -> Self {
-                let ::frunk_core::hlist::hlist_pat!(#(#fnames3 ), *) = r;
-                #name { #(#fnames4    : #fnames5),* }
+                let #hcons_pat = r;
+                #new_struct_constr
             }
         }
     }
 }
 
-
-#[proc_macro_derive(HelloWorld)]
-pub fn hello_world(input: TokenStream) -> TokenStream {
-    // Construct a string representation of the type definition
-    let s = input.to_string();
-
-    // Parse the string representation
-    let ast = syn::parse_macro_input(&s).unwrap();
-
-    // Build the impl
-    let gen = impl_hello_world(&ast);
-
-    // Return the generated impl
-    gen.parse().unwrap()
+fn build_repr(field_types: &Vec<Ty>) -> quote::Tokens {
+    match field_types.len() {
+        0 => quote! { ::frunk_core::hlist::HNil },
+        1 => {
+            let h = field_types[0].clone();
+            quote! { ::frunk_core::hlist::HCons<#h, ::frunk_core::hlist::HNil> }
+        },
+        _ => {
+            let h = field_types[0].clone();
+            let tail = field_types[1..].to_vec();
+            let tail_type = build_repr(&tail);
+            quote! { ::frunk_core::hlist::HCons<#h, #tail_type> }
+        }
+    }
 }
 
-fn impl_hello_world(ast: &syn::MacroInput) -> quote::Tokens {
-    let name = &ast.ident;
-    quote! {
-        impl HelloWorld for #name {
-            fn hello_world() {
-                println!("Hello, World! My name is {}", stringify!(#name));
-            }
+fn build_hcons_constr(field_types: &Vec<Ident>) -> quote::Tokens {
+    match field_types.len() {
+        0 => quote! { ::frunk_core::hlist::HNil },
+        1 => {
+            let h = field_types[0].clone();
+            quote! { ::frunk_core::hlist::HCons{ head: #h, tail: ::frunk_core::hlist::HNil } }
+        },
+        _ => {
+            let h = field_types[0].clone();
+            let tail = field_types[1..].to_vec();
+            let hlist_tail = build_hcons_constr(&tail);
+            quote! { ::frunk_core::hlist::HCons{ head: #h, tail: #hlist_tail }}
         }
+    }
+}
+
+fn build_new_struct_constr(struct_name: &Ident, maybe_fieldnames: &Vec<Option<Ident>>, bindnames: &Vec<Ident>) -> quote::Tokens {
+    let is_tuple_struct = maybe_fieldnames.iter().all(|m_f| m_f.is_none());
+    if is_tuple_struct {
+        let cloned_bind = bindnames.clone();
+        quote! { #struct_name { #(#cloned_bind),* } }
+    } else {
+        let cloned_bind1 = bindnames.clone();
+        let cloned_bind2 = bindnames.clone();
+        quote! { #struct_name { #(#cloned_bind1: #cloned_bind2),* } }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,15 @@
 #[macro_use]
 extern crate frunk_derives;
+extern crate frunk_core;
 
-trait HelloWorld {
-    fn hello_world();
-}
 
-#[derive(Generic, HelloWorld)]
+#[derive(Generic)]
 struct FrenchToast {
     hi: i32,
     bye: i32
 }
 
-#[derive(HelloWorld)]
-struct Waffles;
 
 fn main() {
-    FrenchToast::hello_world();
-    Waffles::hello_world();
+
 }
