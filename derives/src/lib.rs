3521ab1df84064ad8c93f5ca1a098a7adaa0f6a9
Lloyd <lloydmeta@gmail.com>
2017-03-03 00:07:28 +0900

    Add documentation to methods that directly power custom derives

--- derives/src/lib.rs
+++ derives/src/lib.rs
@@ -15,6 +15,8 @@ use derive_generic::impl_generic;
 mod derive_labelled_generic;
 use derive_labelled_generic::impl_labelled_generic;
 
+/// Derives a Generic instance based on HList for
+/// a given Struct or Tuple Struct
 #[proc_macro_derive(Generic)]
 pub fn generic(input: TokenStream) -> TokenStream {
     // Build the impl
@@ -24,6 +26,13 @@ pub fn generic(input: TokenStream) -> TokenStream {
     gen.parse().unwrap()
 }
 
+/// Derives a Generic instance based on Labelled + HList for
+/// a given Struct (Tuple Structs not supported because they have
+/// no labels)
+///
+/// There *may* be problems if you certain characters in your
+/// field names. This can be solved by adding letter to the
+/// create_enums_for! macro invocation in frunk_core::labelled via a PR :)
 #[proc_macro_derive(LabelledGeneric)]
 pub fn labelled_generic(input: TokenStream) -> TokenStream {
     // Build the impl
