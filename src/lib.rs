c63804fe35d42da37e1b2bfcac49213dd4abaffc
Lloyd <lloydmeta@gmail.com>
2017-03-03 02:29:46 +0900

    Add demo of generic and labelled generic to documentation

--- src/lib.rs
+++ src/lib.rs
@@ -16,6 +16,8 @@
 //! # #[macro_use] extern crate frunk_core;
 //! # use frunk_core::hlist::*; fn main() {
 //! use frunk_core::hlist::*;
+//! use frunk_core::generic::*;
+//! use frunk_core::labelled::*;
 //! use frunk::monoid::*;
 //! use frunk::semigroup::*;
 //! use frunk::validated::*;
@@ -34,7 +36,59 @@
 //! let h1 = hlist![Some(1), 3.3, 53i64, "hello".to_owned()];
 //! let h2 = hlist![Some(2), 1.2, 1i64, " world".to_owned()];
 //! let h3 = hlist![Some(3), 4.5, 54, "hello world".to_owned()];
-//! assert_eq!(h1.combine(&h2), h3)
+//! assert_eq!(h1.combine(&h2), h3);
+//!
+//! // Generic and LabelledGeneric-based programming
+//! // Allows Structs to play well easily with HLists
+//!
+//! #[derive(Generic, LabelledGeneric)]
+//! struct ApiUser<'a> {
+//!     FirstName: &'a str,
+//!     LastName: &'a str,
+//!     Age: usize,
+//! }
+//!
+//! #[derive(Generic, LabelledGeneric)]
+//! struct NewUser<'a> {
+//!     first_name: &'a str,
+//!     last_name: &'a str,
+//!     age: usize,
+//! }
+//!
+//! #[derive(Generic, LabelledGeneric)]
+//! struct SavedUser<'a> {
+//!     first_name: &'a str,
+//!     last_name: &'a str,
+//!     age: usize,
+//! }
+//!
+//! // Instantiate a struct from an HList. Note that you can go the other way too.
+//! let a_user: ApiUser = from_generic(hlist!["Joe", "Blow", 30]);
+//!
+//! // Convert using Generic
+//! let n_user = <NewUser as Generic>::convert_from(a_user); // done
+//!
+//! // Convert using LabelledGeneric
+//! //
+//! // This will fail if the fields of the types converted to and from do not
+//! // have the same names or do not line up properly :)
+//! //
+//! // Also note that we're using a helper method to avoid having to use universal
+//! // function call syntax
+//! let s_user: SavedUser = labelled_convert_from(n_user);
+//!
+//! assert_eq!(s_user.first_name, "Joe");
+//! assert_eq!(s_user.last_name, "Blow");
+//! assert_eq!(s_user.age, 30);
+//!
+//! // Uh-oh ! last_name and first_name have been flipped!
+//! #[derive(LabelledGeneric)]
+//! struct DeletedUser<'a> {
+//!     last_name: &'a str,
+//!     first_name: &'a str,
+//!     age: usize,
+//! }
+//! // let d_user = <DeletedUser as LabelledGeneric>::convert_from(s_user); <-- this would fail at compile time :)
 //! # }
 //! ```
 //!
