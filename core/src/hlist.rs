3648a0347973cfda461554b88d72617a45c8a464
Lloyd <lloydmeta@gmail.com>
2017-03-03 22:28:40 +0900

    Add more to the doctest of hlist module

--- core/src/hlist.rs
+++ core/src/hlist.rs
@@ -1,4 +1,4 @@
-//! Module that holds HList data structures and implementations
+//! Module that holds HList data structures, implementations, and typeclasses.
 //!
 //! Typically, you would want to use the `hlist!` macro to make it easier
 //! for you to use HList.
@@ -10,6 +10,42 @@
 //! let (a, b) = h.into_tuple2();
 //! assert_eq!(a, 1);
 //! assert_eq!(b, "hi");
+//!
+//! // Reverse
+//! let h1 = hlist![true, "hi"];
+//! assert_eq!(h1.into_reverse(), hlist!["hi", true]);
+//!
+//! // foldr (foldl also available)
+//! let h2 = hlist![1, false, 42f32];
+//! let folded = h2.foldr(
+//!             hlist![
+//!                 |i, acc| i + acc,
+//!                 |_, acc| if acc > 42f32 { 9000 } else { 0 },
+//!                 |f, acc| f + acc
+//!             ],
+//!             1f32
+//!     );
+//! assert_eq!(folded, 9001);
+//!
+//! // Mapping over an HList
+//! let h3 = hlist![9000, "joe", 41f32];
+//! let mapped = h3.map(hlist![
+//!                         |n| n + 1,
+//!                         |s| s,
+//!                         |f| f + 1f32]);
+//! assert_eq!(mapped, hlist![9001, "joe", 42f32]);
+//!
+//! // Plucking a value out by type
+//! let h4 = hlist![1, "hello", true, 42f32];
+//! let (t, remainder): (bool, _) = h4.pluck();
+//! assert!(t);
+//! assert_eq!(remainder, hlist![1, "hello", 42f32]);
+//!
+//! // Resculpting an HList
+//! let h5 = hlist![9000, "joe", 41f32, true];
+//! let (reshaped, remainder2): (Hlist![f32, i32, &str], _) = h5.sculpt();
+//! assert_eq!(reshaped, hlist![41f32, 9000, "joe"]);
+//! assert_eq!(remainder2, hlist![true]);
 //! # }
 //! ```
 
