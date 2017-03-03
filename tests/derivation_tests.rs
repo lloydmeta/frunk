4d315e55ff0a11b9bb0d755031b7c21a107720ba
Lloyd <lloydmeta@gmail.com>
2017-03-03 17:31:50 +0900

    Add rescult->from_labelled_generic test

--- tests/derivation_tests.rs
+++ tests/derivation_tests.rs
@@ -178,6 +178,41 @@ fn test_mixed_conversions_round_trip() {
 }
 
 
+#[test]
+fn test_reshaped_labelled_generic_conversion() {
+
+    #[derive(LabelledGeneric)]
+    struct NormalUser<'a> {
+        first_name: &'a str,
+        last_name: &'a str,
+        age: usize
+    }
+
+    // Fields are jumbled :(
+    #[derive(LabelledGeneric)]
+    struct JumbledUser<'a> {
+        last_name: &'a str,
+        age: usize,
+        first_name: &'a str,
+    }
+
+    let n_u = NormalUser {
+        first_name: "Moe",
+        last_name: "Ali",
+        age: 30
+    };
+
+    let n_gen = into_labelled_generic(n_u);
+    let jumbled_gen: <JumbledUser as LabelledGeneric>::Repr = n_gen.sculpt();
+
+    let j_u: JumbledUser = from_labelled_generic(jumbled_gen);
+
+    assert_eq!(j_u.first_name, "Moe");
+    assert_eq!(j_u.last_name, "Ali");
+    assert_eq!(j_u.age, 30)
+}
+
+
 // Working with Validated
 
 #[derive(PartialEq, Eq, Debug)]
