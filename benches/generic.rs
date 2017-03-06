616270521330038b5152579e8514b7a97ba9cdbe
Lloyd <lloydmeta@gmail.com>
2017-03-06 11:26:24 +0900

    Add benchmarks for generic, labelled generic conversion and name retrieval

---
+++ benches/generic.rs
@@ -0,0 +1,35 @@
+#![feature(test)]
+
+#[macro_use]
+extern crate frunk;
+extern crate frunk_core;
+extern crate test;
+
+use frunk::*;
+use test::Bencher;
+
+#[derive(Generic)]
+struct NewUser<'a> {
+    first_name: &'a str,
+    last_name: &'a str,
+    age: usize
+}
+
+#[derive(Generic)]
+struct SavedUser<'a> {
+    first_name: &'a str,
+    last_name: &'a str,
+    age: usize
+}
+
+#[bench]
+fn generic_conversion(b: &mut Bencher) {
+    b.iter(|| {
+        let n_u = NewUser {
+            first_name: "Joe",
+            last_name: "Schmoe",
+            age: 30
+        };
+        <SavedUser as Generic>::convert_from(n_u)
+    })
+}
---
+++ benches/labelled.rs
@@ -0,0 +1,44 @@
+#![feature(test)]
+
+#[macro_use]
+extern crate frunk;
+#[macro_use]
+extern crate frunk_core;
+extern crate test;
+
+use frunk::*;
+use test::Bencher;
+
+#[derive(LabelledGeneric)]
+struct NewUser<'a> {
+    first_name: &'a str,
+    last_name: &'a str,
+    age: usize
+}
+
+#[derive(LabelledGeneric)]
+struct SavedUser<'a> {
+    first_name: &'a str,
+    last_name: &'a str,
+    age: usize
+}
+
+#[bench]
+fn labelled_conversion(b: &mut Bencher) {
+    b.iter(|| {
+        let n_u = NewUser {
+            first_name: "Joe",
+            last_name: "Schmoe",
+            age: 30
+        };
+        <SavedUser as LabelledGeneric>::sculpted_convert_from(n_u)
+    })
+}
+
+#[bench]
+fn name(b: &mut Bencher) {
+    let field = label::<Hlist![f,i,r,s,t,__,n,a,m,e], _>(30);
+    b.iter(|| {
+        field.name()
+    })
+}
