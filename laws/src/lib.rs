e94e7733aeb82f05d0264bc3bf8ef538416472b2
Lloyd <lloydmeta@gmail.com>
2017-03-24 14:07:06 +0900

    Start adding Laws submodule

---
+++ laws/src/lib.rs
@@ -0,0 +1,6 @@
+#![doc(html_playground_url = "https://play.rust-lang.org/")]
+//! Frunk Laws
+//!
+//! This library contains laws that can be used to test the implementation of
+//! algebras declared in Frunk
+
