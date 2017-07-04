9e485d5aa3aad41b7e42b424245bbf9b57dbc11f
Lloyd <lloydmeta@gmail.com>
2017-07-04 11:20:34 +0900

    Add examples
    * Add examples to check for usage
    * Add building of new examples to travis

---
+++ examples/labelled.rs
@@ -0,0 +1,45 @@
+#[macro_use] // for the hlist macro
+extern crate frunk;
+extern crate frunk_core;
+use frunk::labelled::*; // for the Generic trait and HList
+
+#[derive(LabelledGeneric)]
+struct NewUser<'a> {
+    first_name: &'a str,
+    last_name: &'a str,
+    age: usize,
+}
+
+#[derive(LabelledGeneric)]
+struct SavedUser<'a> {
+    first_name: &'a str,
+    last_name: &'a str,
+    age: usize,
+}
+
+#[derive(LabelledGeneric)]
+struct DeletedUser<'a> {
+    last_name: &'a str,
+    first_name: &'a str,
+    age: usize,
+}
+
+fn main() {
+
+    let n_user = NewUser {
+        first_name: "Joe",
+        last_name: "Blow",
+        age: 30,
+    };
+
+    let s_user: SavedUser = labelled_convert_from(n_user);
+
+    assert_eq!(s_user.first_name, "Joe");
+    assert_eq!(s_user.last_name, "Blow");
+    assert_eq!(s_user.age, 30);
+
+    let d_user: DeletedUser = transform_from(s_user);
+
+    assert_eq!(d_user.first_name, "Joe");
+    println!("{}", p.last_name);
+}
