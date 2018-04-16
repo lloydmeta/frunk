13339594580dffb371babd698e6cda53f08f55cb
Mazdak Farrokhzad <twingoow@gmail.com>
2018-04-16 04:23:08 +0200

    add usage example for map_repr.

--- examples/generic.rs
+++ examples/generic.rs
@@ -10,15 +10,21 @@ struct Person<'a> {
 }
 
 fn main() {
-    let h = hlist!("Joe", "Blow", 30);
-    let p: Person = frunk::from_generic(h);
+    let repr = hlist!("Joe", "Blow", 30);
+    let person: Person = frunk::from_generic(repr);
     assert_eq!(
-        p,
+        person,
         Person {
             first_name: "Joe",
             last_name: "Blow",
             age: 30,
         }
     );
-    println!("{}", p.first_name);
+    println!("{}", person.first_name);
+
+    let older_person = frunk::map_repr(person, |repr| {
+        let hlist_pat![first, last, age] = repr;
+        hlist![first, last, age * 3]
+    });
+    assert_eq!(older_person.age, 90);
 }
