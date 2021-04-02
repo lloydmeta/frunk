ffd4649385b3d6dea89580fdcc4612a9ecd1476e
lloydmeta <lloydmeta@users.noreply.github.com>
2021-04-02 03:22:43 +0000

    deploy: 7fa860ca8523ac4fff3096b327276d01b28a26a3

--- implementors/frunk/semigroup/trait.Semigroup.js
+++ implementors/frunk/semigroup/trait.Semigroup.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {};
 implementors["frunk"] = [];
-implementors["frunk_laws"] = [{"text":"impl&lt;A:&nbsp;Semigroup&gt; Semigroup for <a class=\"struct\" href=\"frunk_laws/wrapper/struct.Wrapper.html\" title=\"struct frunk_laws::wrapper::Wrapper\">Wrapper</a>&lt;A&gt;","synthetic":false,"types":["frunk_laws::wrapper::Wrapper"]}];
+implementors["frunk_laws"] = [{"text":"impl&lt;A:&nbsp;<a class=\"trait\" href=\"frunk/semigroup/trait.Semigroup.html\" title=\"trait frunk::semigroup::Semigroup\">Semigroup</a>&gt; <a class=\"trait\" href=\"frunk/semigroup/trait.Semigroup.html\" title=\"trait frunk::semigroup::Semigroup\">Semigroup</a> for <a class=\"struct\" href=\"frunk_laws/wrapper/struct.Wrapper.html\" title=\"struct frunk_laws::wrapper::Wrapper\">Wrapper</a>&lt;A&gt;","synthetic":false,"types":["frunk_laws::wrapper::Wrapper"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
