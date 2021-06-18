8d58fd60f5d5379e9b80908375a5b28c1c61a024
lloydmeta <lloydmeta@gmail.com>
2021-06-18 13:10:13 +0900

    Use β instead for less confusion.

--- tests/labelled_tests.rs
+++ tests/labelled_tests.rs
@@ -114,7 +114,7 @@ fn test_transmogrify() {
             },
         },
         is_banned: true,
-        α: true,
+        β: true,
     };
     let expected_external_user = ExternalUser {
         name: "John",
@@ -123,7 +123,7 @@ fn test_transmogrify() {
             name: "somewhere out there",
             phone: ExternalPhoneNumber { main: 1234 },
         },
-        α: true,
+        β: true,
     };
     let external_user: ExternalUser = internal_user.transmogrify();
     assert_eq!(external_user, expected_external_user);
