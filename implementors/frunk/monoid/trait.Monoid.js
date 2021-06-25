6353092409d5848c47b0be6e8e8cc478754cd88a
lloydmeta <lloydmeta@users.noreply.github.com>
2021-06-25 01:44:08 +0000

    deploy: f83e1d8c2e4e6d080cc646ffc9456eb90f71960a

--- implementors/frunk/monoid/trait.Monoid.js
+++ implementors/frunk/monoid/trait.Monoid.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {};
 implementors["frunk"] = [];
-implementors["frunk_laws"] = [{"text":"impl&lt;A:&nbsp;<a class=\"trait\" href=\"frunk/monoid/trait.Monoid.html\" title=\"trait frunk::monoid::Monoid\">Monoid</a>&gt; <a class=\"trait\" href=\"frunk/monoid/trait.Monoid.html\" title=\"trait frunk::monoid::Monoid\">Monoid</a> for <a class=\"struct\" href=\"frunk_laws/wrapper/struct.Wrapper.html\" title=\"struct frunk_laws::wrapper::Wrapper\">Wrapper</a>&lt;A&gt;","synthetic":false,"types":["frunk_laws::wrapper::Wrapper"]}];
+implementors["frunk_laws"] = [{"text":"impl&lt;A:&nbsp;Monoid&gt; Monoid for <a class=\"struct\" href=\"frunk_laws/wrapper/struct.Wrapper.html\" title=\"struct frunk_laws::wrapper::Wrapper\">Wrapper</a>&lt;A&gt;","synthetic":false,"types":["frunk_laws::wrapper::Wrapper"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
