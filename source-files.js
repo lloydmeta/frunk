9d6311862e8b86f983cf5382fd7803e2104c70ec
lloydmeta <lloydmeta@users.noreply.github.com>
2022-10-02 04:47:39 +0000

    deploy: 573f44f8e61ed461ea3e0214a2dd1b3b7a0b7402

--- source-files.js
+++ source-files.js
@@ -1,9 +1,10 @@
-var sourcesIndex = {};
-sourcesIndex["frunk"] = {"name":"","files":["lib.rs","monoid.rs","semigroup.rs","validated.rs"]};
-sourcesIndex["frunk_core"] = {"name":"","files":["coproduct.rs","generic.rs","hlist.rs","indices.rs","labelled.rs","lib.rs","macros.rs","path.rs","traits.rs","tuples.rs"]};
-sourcesIndex["frunk_derives"] = {"name":"","files":["derive_generic.rs","derive_labelled_generic.rs","lib.rs"]};
-sourcesIndex["frunk_laws"] = {"name":"","files":["lib.rs","monoid_laws.rs","semigroup_laws.rs","wrapper.rs"]};
-sourcesIndex["frunk_proc_macro_helpers"] = {"name":"","files":["lib.rs"]};
-sourcesIndex["frunk_proc_macros"] = {"name":"","files":["lib.rs"]};
-sourcesIndex["frunk_proc_macros_impl"] = {"name":"","files":["lib.rs"]};
+var sourcesIndex = JSON.parse('{\
+"frunk":["",[],["lib.rs","monoid.rs","semigroup.rs","validated.rs"]],\
+"frunk_core":["",[],["coproduct.rs","generic.rs","hlist.rs","indices.rs","labelled.rs","lib.rs","macros.rs","path.rs","traits.rs","tuples.rs"]],\
+"frunk_derives":["",[],["derive_generic.rs","derive_labelled_generic.rs","lib.rs"]],\
+"frunk_laws":["",[],["lib.rs","monoid_laws.rs","semigroup_laws.rs","wrapper.rs"]],\
+"frunk_proc_macro_helpers":["",[],["lib.rs"]],\
+"frunk_proc_macros":["",[],["lib.rs"]],\
+"frunk_proc_macros_impl":["",[],["lib.rs"]]\
+}');
 createSourceSidebar();
