c5bb9818c3741d1f4983bd5b75cfa3bb61803a4c
lloydmeta <lloydmeta@users.noreply.github.com>
2024-02-10 13:54:38 +0000

    deploy: e24c63dffd69ba6ba56c2b1d3688267c69b06f41

---
+++ trait.impl/frunk/semigroup/trait.Semigroup.js
@@ -0,0 +1,4 @@
+(function() {var implementors = {
+"frunk":[],
+"frunk_laws":[["impl&lt;A: <a class=\"trait\" href=\"frunk/semigroup/trait.Semigroup.html\" title=\"trait frunk::semigroup::Semigroup\">Semigroup</a>&gt; <a class=\"trait\" href=\"frunk/semigroup/trait.Semigroup.html\" title=\"trait frunk::semigroup::Semigroup\">Semigroup</a> for <a class=\"struct\" href=\"frunk_laws/wrapper/struct.Wrapper.html\" title=\"struct frunk_laws::wrapper::Wrapper\">Wrapper</a>&lt;A&gt;"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
