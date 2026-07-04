62380535f8387b0785a33317634206e4581c7cfa
lloydmeta <lloydmeta@users.noreply.github.com>
2026-07-04 04:13:07 +0000

    deploy: f5deeb1ad9f9e98c2d46b67c7412abc2a767396a

--- trait.impl/frunk/monoid/trait.Monoid.js
+++ trait.impl/frunk/monoid/trait.Monoid.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["frunk",[]],["frunk_laws",[["impl&lt;A: Monoid&gt; Monoid for <a class=\"struct\" href=\"frunk_laws/wrapper/struct.Wrapper.html\" title=\"struct frunk_laws::wrapper::Wrapper\">Wrapper</a>&lt;A&gt;",0]]]]);
+    const implementors = Object.fromEntries([["frunk",[]],["frunk_laws",[["impl&lt;A: <a class=\"trait\" href=\"frunk/monoid/trait.Monoid.html\" title=\"trait frunk::monoid::Monoid\">Monoid</a>&gt; <a class=\"trait\" href=\"frunk/monoid/trait.Monoid.html\" title=\"trait frunk::monoid::Monoid\">Monoid</a> for <a class=\"struct\" href=\"frunk_laws/wrapper/struct.Wrapper.html\" title=\"struct frunk_laws::wrapper::Wrapper\">Wrapper</a>&lt;A&gt;",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[12,191]}
+//{"start":59,"fragment_lengths":[12,393]}
--- trait.impl/frunk/semigroup/trait.Semigroup.js
+++ trait.impl/frunk/semigroup/trait.Semigroup.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["frunk",[]],["frunk_laws",[["impl&lt;A: Semigroup&gt; Semigroup for <a class=\"struct\" href=\"frunk_laws/wrapper/struct.Wrapper.html\" title=\"struct frunk_laws::wrapper::Wrapper\">Wrapper</a>&lt;A&gt;",0]]]]);
+    const implementors = Object.fromEntries([["frunk",[]],["frunk_laws",[["impl&lt;A: <a class=\"trait\" href=\"frunk/semigroup/trait.Semigroup.html\" title=\"trait frunk::semigroup::Semigroup\">Semigroup</a>&gt; <a class=\"trait\" href=\"frunk/semigroup/trait.Semigroup.html\" title=\"trait frunk::semigroup::Semigroup\">Semigroup</a> for <a class=\"struct\" href=\"frunk_laws/wrapper/struct.Wrapper.html\" title=\"struct frunk_laws::wrapper::Wrapper\">Wrapper</a>&lt;A&gt;",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[12,197]}
+//{"start":59,"fragment_lengths":[12,423]}
